b7caa77534430b858df5cf82732aa23207515755
rickysilitonga112 <rickysilitonga112@gmail.com>
2023-12-29 17:48:02 +0700

    Initial Commit

---
+++ dattebayo-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dattebayo-ios
+//
+//  Created by Ricky Silitonga on 29/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
